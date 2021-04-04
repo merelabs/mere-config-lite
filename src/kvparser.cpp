93a9f6de95c35c04032edb50fda8c09edecc9dd2
Bhuiyan Mohammad Iklash <bhuiyan.mohammad.iklash@gmail.com>
2021-04-04 14:02:25 +0600

    Fix key retrival

--- src/kvparser.cpp
+++ src/kvparser.cpp
@@ -1,10 +1,12 @@
 #include "kvparser.h"
+#include "exception.h"
+
 #include "mere/utils/stringutils.h"
 
 Mere::Config::KVParser::KVParser(const KVConfig &config, QObject *parent)
     : Parser(config, parent)
 {
-    connect(this, SIGNAL(parsed(const std::string &) const), this, SLOT(parse(const std::string &)));
+
 }
 
 std::vector<Mere::Config::Property> Mere::Config::KVParser::parse() const
@@ -15,7 +17,11 @@ std::vector<Mere::Config::Property> Mere::Config::KVParser::parse() const
     for(const std::string &line : lines)
     {
         std::string key   = this->key(line);
-        if(key.empty()) continue;
+        if(key.empty())
+        {
+            if (strict()) throw Exception("malformed configuration");
+            continue;
+        }
 
         std::string value = this->value(line);
 
@@ -27,11 +33,13 @@ std::vector<Mere::Config::Property> Mere::Config::KVParser::parse() const
 
 std::string Mere::Config::KVParser::parse(const std::string &key, int *set) const
 {
-    std::string value = Parser::parse(key, set);
+    std::string match(key);
+    match.append("=");
 
-    if (set) *set = !value.empty();
+    std::string line = Parser::parse(match, set);
+    if (line.empty()) return line;
 
-    return value;
+    return this->value(line);
 }
 
 std::string Mere::Config::KVParser::key(const std::string &line) const
